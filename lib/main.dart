5a2c9b390df00f70162df9f7325d83073a3fd3b4
madhankumar-ch <chiluverimadhankumarnetha@gmail.com>
2024-09-20 10:45:55 +0530

    glb added

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:warehouse_3d/bloc/warehouse_interaction_bloc.dart';
 import 'package:warehouse_3d/inits/init.dart';
+import 'package:warehouse_3d/pages/3d_rack.dart';
 import 'package:warehouse_3d/pages/warehouse_test.dart';
 
 void main() async {
@@ -18,8 +19,7 @@ void main() async {
     child: MaterialApp(
       theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.white, primary: Colors.black)),
       debugShowCheckedModeBanner: false,
-      home: const WarehouseTest(),
+      home: const ThreeDRack(),
     ),
   ));
 }
-
