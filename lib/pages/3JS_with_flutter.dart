486c7b4e9c52af155eb5ef6710a9e1043de0073b
madhankumar-ch <chiluverimadhankumarnetha@gmail.com>
2024-09-25 23:22:31 +0530

    achieved on hover color change and on click color change using raycasting.

--- lib/pages/3JS_with_flutter.dart
+++ lib/pages/3JS_with_flutter.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
@@ -41,6 +43,25 @@ class _ThreeDTestState extends State<ThreeDTest> {
                     ),
                     onWebViewCreated: (controller) {
                       print('created');
+                      controller.evaluateJavascript(source: 'test()');
+                    },
+                    onLoadStop: (controller, url) async {
+                      if (!Platform.isAndroid) {
+                        // wait until the page is loaded, and then create the Web Message Channel
+                        var webMessageChannel = await controller.createWebMessageChannel();
+                        var port1 = webMessageChannel!.port1;
+                        var port2 = webMessageChannel.port2;
+  
+                        // set the web message callback for the port1
+                        await port1.setWebMessageCallback((message) async {
+                          print("Message coming from the JavaScript side: $message");
+                          // when it receives a message from the JavaScript side, respond back with another message.
+                          await port1.postMessage(WebMessage(data: "${message!} and back"));
+                        });
+  
+                        // transfer port2 to the webpage to initialize the communication
+                        await controller.postWebMessage(message: WebMessage(data: "capturePort", ports: [port2]), targetOrigin: WebUri.uri(Uri.parse("*")));
+                      }
                     },
                   ),
                 );
