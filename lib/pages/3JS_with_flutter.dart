3571c54fdb70e82f581b77f6006ee01af80bf632
madhankumar-ch <chiluverimadhankumarnetha@gmail.com>
2024-09-26 14:45:53 +0530

    added click functionality

--- lib/pages/3JS_with_flutter.dart
+++ lib/pages/3JS_with_flutter.dart
@@ -1,3 +1,4 @@
+import 'dart:collection';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
@@ -29,40 +30,13 @@ class _ThreeDTestState extends State<ThreeDTest> {
       body: FutureBuilder(
           future: loadWebCode(),
           builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              if (snapshot.hasData) {
+            if (true) {
+              if (true) {
                 return Container(
                   height: size.height,
                   width: size.width,
                   child: InAppWebView(
-                    initialUrlRequest: URLRequest(
-                      url: WebUri.uri(Uri.parse('assets/web_code/model1.html'))
-                    ),
-                    initialSettings: InAppWebViewSettings(
-                      
-                    ),
-                    onWebViewCreated: (controller) {
-                      print('created');
-                      controller.evaluateJavascript(source: 'test()');
-                    },
-                    onLoadStop: (controller, url) async {
-                      if (!Platform.isAndroid) {
-                        // wait until the page is loaded, and then create the Web Message Channel
-                        var webMessageChannel = await controller.createWebMessageChannel();
-                        var port1 = webMessageChannel!.port1;
-                        var port2 = webMessageChannel.port2;
-  
-                        // set the web message callback for the port1
-                        await port1.setWebMessageCallback((message) async {
-                          print("Message coming from the JavaScript side: $message");
-                          // when it receives a message from the JavaScript side, respond back with another message.
-                          await port1.postMessage(WebMessage(data: "${message!} and back"));
-                        });
-  
-                        // transfer port2 to the webpage to initialize the communication
-                        await controller.postWebMessage(message: WebMessage(data: "capturePort", ports: [port2]), targetOrigin: WebUri.uri(Uri.parse("*")));
-                      }
-                    },
+                    initialFile: 'assets/web_code/model1.html',
                   ),
                 );
               } else {
