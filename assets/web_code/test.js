e4dda8961b21b2ae222fd3037f6439538882c27b
madhankumar-ch <chiluverimadhankumarnetha@gmail.com>
2024-10-16 23:00:24 +0530

    added racks using three js code

--- assets/web_code/test.js
+++ assets/web_code/test.js
@@ -3,7 +3,7 @@ import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@latest/examples/j
 import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@latest/examples/jsm/controls/OrbitControls.js";
 
 //Global Variables
-var env, controls;
+var env, controls, rack = new THREE.Object3D(), warehouseDepth, warehouseWidth;
 
 document.addEventListener("DOMContentLoaded", function () {
   env = new Environment(
@@ -12,9 +12,13 @@ document.addEventListener("DOMContentLoaded", function () {
       75,
       window.innerWidth / window.innerHeight,
       0.1,
-      1000
+      3000
     ),
-    new THREE.WebGLRenderer()
+    new THREE.WebGLRenderer({
+      antialias: true, alpha: true,
+      logarithmicDepthBuffer: true,
+      preserveDrawingBuffer: true,
+    })
   );
 
   // Environment Setup
@@ -27,8 +31,9 @@ document.addEventListener("DOMContentLoaded", function () {
   env.addRendererToDocument();
 
   buildGround();
-  buildCompundWall();
-  buildWarehouseWall();
+  buildCompund();
+  buildWarehouse();
+  addRacks();
 
   lightSetup();
 
@@ -43,26 +48,27 @@ document.addEventListener("DOMContentLoaded", function () {
     env.renderer.render(env.scene, env.camera);
   }
 
-  //ground model
-  function buildGround() {
+  function buildPlane(width, depth, textureImg, repeatHorizontal, repeatVertical) {
     // Create Plane Geometry
-    const width = 700; // Width of the plane
-    const height = 700; // Height of the plane
-    const geometry = new THREE.PlaneGeometry(width, height);
+    const geometry = new THREE.PlaneGeometry(width, depth);
 
-    // Load Texture
-    const textureLoader = new THREE.TextureLoader();
-    const texture = textureLoader.load("../images/ground.png", function () {
-      // Set texture repeat
-      texture.wrapS = THREE.RepeatWrapping; // Repeat horizontally
-      texture.wrapT = THREE.RepeatWrapping; // Repeat vertically
-      texture.repeat.set(10, 10); // Number of times to repeat in each direction
-    }); // Replace with your texture path
+    var texture;
+
+    if (textureImg) {
+      // Load Texture
+      const textureLoader = new THREE.TextureLoader();
+      texture = textureLoader.load(textureImg, function () {
+        // Set texture repeat
+        texture.wrapS = THREE.RepeatWrapping; // Repeat horizontally
+        texture.wrapT = THREE.RepeatWrapping; // Repeat vertically
+        texture.repeat.set(repeatHorizontal, repeatVertical); // Number of times to repeat in each direction
+      }); // Replace with your texture path
+    }
 
     // Create Material with Texture
     const material = new THREE.MeshStandardMaterial({
-      map: texture,
-      metalness: 0.5,
+      map: textureImg != null ? texture : null,
+      metalness: 0.1,
       roughness: 0.5,
     });
 
@@ -73,11 +79,16 @@ document.addEventListener("DOMContentLoaded", function () {
     // Rotate Plane to be perpendicular to Y-axis
     plane.rotation.x = -Math.PI / 2;
 
-    // Add Plane to Scene
-    env.scene.add(plane);
+    return plane;
   }
 
-  function buildCompundWall() {
+  //ground model
+  function buildGround() {
+    // Add Plane to Scene
+    env.scene.add(buildPlane(900, 700, "../images/ground.png", 10, 10));
+  }
+
+  function buildCompund() {
     // Define wall dimensions
     const wallHeight = 36; // Height of the walls
     const wallThickness = 2; // Thickness of the walls
@@ -95,7 +106,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Create Walls
     const wallMaterial = new THREE.MeshStandardMaterial({
       map: texture,
-      metalness: 0.5,
+      metalness: 0.1,
       roughness: 0.5,
     });
 
@@ -135,14 +146,27 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Add Walls to Scene
     env.scene.add(compundWalls);
+
+    const flooring = buildPlane(roomWidth, roomDepth, "../images/compound_flooring.jpg", 50, 100);
+
+    flooring.position.set(0, 0.005, 0);
+
+    flooring.rotation.x = -Math.PI / 2;
+
+    env.scene.add(flooring);
+  }
+
+  function buildWarehouse() {
+    buildWarehouseWall();
+    buildWarehouseAreas();
   }
 
   function buildWarehouseWall() {
     // Define wall dimensions
     const wallHeight = 24; // Height of the walls
     const wallThickness = 2; // Thickness of the walls
-    const roomWidth = 200; // Width of the room
-    const roomDepth = 120; // Depth of the room
+    warehouseWidth = 200; // Width of the room
+    warehouseDepth = 120; // Depth of the room
 
     // Load Texture
     const textureLoader = new THREE.TextureLoader();
@@ -159,56 +183,139 @@ document.addEventListener("DOMContentLoaded", function () {
     // Create Walls
     const wallMaterial = new THREE.MeshStandardMaterial({
       map: texture,
-      metalness: 0.5,
+      metalness: 0.1,
       roughness: 0.5,
     });
 
     const warehouseWalls = new THREE.Group();
 
+    
     // Front Wall
-    const frontWall = new THREE.Mesh(
-      new THREE.BoxGeometry(roomWidth + 2, wallHeight, wallThickness),
+    const frontWallLeft = new THREE.Mesh(
+      new THREE.BoxGeometry(warehouseWidth/3 + 2, wallHeight, wallThickness),
       wallMaterial
     );
-    frontWall.position.set(0, wallHeight / 2, -roomDepth / 2); // Position at front
-
+    frontWallLeft.position.set(-warehouseWidth/3, wallHeight / 2, warehouseDepth / 2); // Position at front
+    const frontWallRight = new THREE.Mesh(
+      new THREE.BoxGeometry(warehouseWidth/3 + 2, wallHeight, wallThickness),
+      wallMaterial
+    );
+    frontWallRight.position.set(warehouseWidth/3, wallHeight / 2, warehouseDepth / 2); // Position at front
+    const frontWallMiddle = new THREE.Mesh(
+      new THREE.BoxGeometry(warehouseWidth/3 + 2, wallHeight, wallThickness),
+      wallMaterial
+    );
+    frontWallMiddle.position.set(0, wallHeight / 2, warehouseDepth / 2-10); // Position at front
+    const frontWallLeftTurn = new THREE.Mesh(
+      new THREE.BoxGeometry(wallThickness,wallHeight,8),
+      wallMaterial
+    );
+    frontWallLeftTurn.position.set(-warehouseWidth/6, wallHeight / 2, warehouseDepth / 2-5); // Position at front
+    const frontWallRightTurn = new THREE.Mesh(
+      new THREE.BoxGeometry(wallThickness,wallHeight,8),
+      wallMaterial
+    );
+    frontWallRightTurn.position.set(warehouseWidth/6, wallHeight / 2, warehouseDepth / 2-5); // Position at front
+    warehouseWalls.add(frontWallLeft);
+    warehouseWalls.add(frontWallRight);
+    warehouseWalls.add(frontWallMiddle);
+    warehouseWalls.add(frontWallLeftTurn);
+    warehouseWalls.add(frontWallRightTurn);
+    
     // Back Wall
     const backWall = new THREE.Mesh(
-      new THREE.BoxGeometry(roomWidth + 2, wallHeight, wallThickness),
+      new THREE.BoxGeometry(warehouseWidth + 2, wallHeight, wallThickness),
       wallMaterial
     );
-    backWall.position.set(0, wallHeight / 2, roomDepth / 2); // Position at back
+    backWall.position.set(0, wallHeight / 2, -warehouseDepth / 2); // Position at back
+    warehouseWalls.add(backWall);
 
     // Left Wall
     const leftWall = new THREE.Mesh(
-      new THREE.BoxGeometry(wallThickness, wallHeight, roomDepth),
+      new THREE.BoxGeometry(wallThickness, wallHeight, warehouseDepth),
       wallMaterial
     );
-    leftWall.position.set(-roomWidth / 2, wallHeight / 2, 0); // Position on left
+    leftWall.position.set(-warehouseWidth / 2, wallHeight / 2, 0); // Position on left
+    warehouseWalls.add(leftWall);
 
     // Right Wall
     const rightWall = new THREE.Mesh(
-      new THREE.BoxGeometry(wallThickness, wallHeight, roomDepth),
+      new THREE.BoxGeometry(wallThickness, wallHeight, warehouseDepth),
       wallMaterial
     );
-    rightWall.position.set(roomWidth / 2, wallHeight / 2, 0); // Position on right
+    rightWall.position.set(warehouseWidth / 2, wallHeight / 2, 0); // Position on right
+    warehouseWalls.add(rightWall);
+
+    warehouseWalls.position.set(-warehouseDepth / 3, 0, -warehouseWidth / 4);
 
     // Add Walls to Scene
-    env.scene.add(frontWall);
-    env.scene.add(backWall);
-    env.scene.add(leftWall);
-    env.scene.add(rightWall);
+    env.scene.add(warehouseWalls);
+  }
+
+  function buildWarehouseAreas() {
+    const storageArea = buildPlane(130, 50);
+    console.log('area');
+    storageArea.material.color.set(0xff0000);
+    storageArea.position.set(-74, 0.01, -84);
+    env.scene.add(storageArea);
+  }
+
+  function addRacks() {
+    const Loader = new GLTFLoader();
+
+    // Configure the loader to load textures
+    Loader.loadTexture = true;
+    Loader.load(
+      "../glbs/rack.glb",
+      function (gltf) {
+        rack = gltf.scene;
+
+        // Compute bounding box
+        const box = new THREE.Box3().setFromObject(rack);
+        const dimensions = new THREE.Vector3();
+        box.getSize(dimensions);
+
+        const numberOfRacks = 5; // Number of racks to clone
+        const spacing = dimensions.x * 3; // Distance between each rack in the x-axis
+
+        for (let i = 0; i < numberOfRacks; i++) {
+          // Clone the original rack
+          const rackClone = rack.clone();
+
+          rackClone.traverse((obj) => {
+            if (obj.isMesh) {
+              if (obj.name.includes("rack")) {
+                obj.name = 'rack' + i;
+              }
+            }
+          });
+
+          // console.log(dumpObject(rackClone).join('\n'));
+
+          // Set position for each clone
+          rackClone.position.set(-warehouseWidth * 11 / 20 + i * spacing, 0, -warehouseDepth * 2 / 3);
+
+          // Add cloned rack to the scene
+          env.scene.add(rackClone);
+        }
+
+        env.scene.updateMatrixWorld(true);
+      },
+      undefined,
+      function (error) {
+        console.error(error.toString());
+      }
+    );
   }
 
   function lightSetup() {
     // Add ambient light
     const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-    ambientLight.castShadow = false; // Soft white light
     env.scene.add(ambientLight);
 
     // // Add directional light
     const directionalLight = new THREE.DirectionalLight(0xffffff, 1); // Bright white light
-    directionalLight.position.set(5, 5, 5); // Position the light
+    directionalLight.position.set(0, 100, 100); // Position the light
     env.scene.add(directionalLight);
   }
 
@@ -259,6 +366,21 @@ document.addEventListener("DOMContentLoaded", function () {
     // Initial call to set target within bounds if necessary
     clampTarget();
   }
+
+  function dumpObject(obj, lines = [], isLast = true, prefix = "") {
+    const localPrefix = isLast ? "└─" : "├─";
+    lines.push(
+      `${prefix}${prefix ? localPrefix : ""}${obj.name || "*no-name*"} [${obj.type
+      }]`
+    );
+    const newPrefix = prefix + (isLast ? "  " : "│ ");
+    const lastNdx = obj.children.length - 1;
+    obj.children.forEach((child, ndx) => {
+      const isLast = ndx === lastNdx;
+      dumpObject(child, lines, isLast, newPrefix);
+    });
+    return lines;
+  }
 });
 
 class Environment {
@@ -274,10 +396,10 @@ class Environment {
   cameraSetup() {
     this.camera.position.z = 500;
     this.camera.position.y = 500;
-    this.camera.far = 3000;
   }
 
   addRendererToDocument() {
+    this.renderer.setPixelRatio(Math.min(Math.max(1, window.devicePixelRatio), 2));
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(this.renderer.domElement);
   }
