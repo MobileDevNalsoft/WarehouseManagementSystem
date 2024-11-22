16fe8d1eaf867300e18592854cff50c57a77e2bb
saisanjit <saisanjit.d@gmail.com>
2024-11-22 15:29:55 +0530

    actions js

--- web/actions.js
+++ web/actions.js
@@ -12,11 +12,23 @@ const init = () => {
     const resetTrucks = () => {
         window.localStorage.setItem("resetTrucks", "true")
     }
+    const highlightBins = (bins) => {
+        window.localStorage.setItem("highlightBins", bins);
+    }
+    const resetBoxColors = () => {
+        window.localStorage.setItem("resetBoxColors",true);
+    }
     
+    const navigateToBin = (bin)=>{
+        window.localStorage.setItem("navigateToBin", bin);
+    }
     window._switchToMainCam = switchToMainCam;
     window._isRackDataLoaded = isRacksDataLoaded;
     window._setNumberOfTrucks = setNumberOfTrucks;
     window._resetTrucks = resetTrucks;
+    window._highlightBins = highlightBins;
+    window._navigateToBin = navigateToBin;
+    window._resetBoxColors = resetBoxColors;
 }
 
 window.onload = () => {
