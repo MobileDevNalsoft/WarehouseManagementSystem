54392b0b68d19f1e864b1e653c26d5cda6584b0c
Sai Sanjit Dubbaka <saisanjit.d@gmail.com>
2025-02-27 16:53:11 +0530

    Added maps feature

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:wmssimulator/bloc/authentication/authentication_bloc.dart';
 import 'package:wmssimulator/bloc/container_management/container_bloc.dart';
 import 'package:wmssimulator/bloc/dashboards/dashboard_bloc.dart';
 import 'package:wmssimulator/bloc/dock_area/dock_area_bloc.dart';
+import 'package:wmssimulator/bloc/geo_location/geo_location_bloc.dart';
 import 'package:wmssimulator/bloc/receiving/receiving_bloc.dart';
 import 'package:wmssimulator/bloc/staging/staging_bloc.dart';
 import 'package:wmssimulator/bloc/storage/storage_bloc.dart';
@@ -48,7 +49,8 @@ main() async {
       BlocProvider(create: (_) => StorageBloc(customApi: getIt())),
       BlocProvider(create: (_) => WorkflowBloc(customApi: getIt())),
       BlocProvider(create: (_) => ContainerBloc(customApi: getIt())),
-      BlocProvider(create: (_) => WorkQueueBloc(customApi: getIt()))
+      BlocProvider(create: (_) => WorkQueueBloc(customApi: getIt())),
+      BlocProvider(create: (_) => GeoLocationBloc())
     ],
     child: MaterialApp(
       navigatorKey: getIt<NavigatorService>().navigatorkey,
